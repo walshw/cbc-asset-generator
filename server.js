bacc3bcd9314cdd9ba2a95024c3bde29fd23af14
walshw <williamdouglaswalsh@gmail.com>
2025-03-14 16:52:40 -0400

    Do not need node stuff for gh-pages, removing

--- server.js
+++
@@ -1,7 +0,0 @@
-app.get('/', (req, res) => {
-    res.send(`
-    <form action="/upload" enctype="multipart/form-data" method="POST">
-      <input type="file" name="myFile" multiple/>
-      <input type="submit" value="Upload your files"/>
-    </form>`);
-});
